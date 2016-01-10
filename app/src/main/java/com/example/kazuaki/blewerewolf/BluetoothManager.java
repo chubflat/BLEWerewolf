6e7f2b67d5947fd743b4878f796c6748cd4ac236
chubflat <khasebe1115@gmail.com>
2016-01-10 12:11:00 +0900

    first commit

---
+++ app/src/main/java/com/example/kazuaki/blewerewolf/BluetoothManager.java
@@ -0,0 +1,22 @@
+package com.example.kazuaki.blewerewolf;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+
+/**
+ * Created by Kazuaki on 2016/01/09.
+ */
+public class BluetoothManager extends Activity {
+    BluetoothAdapter Bt = BluetoothAdapter.getDefaultAdapter();
+//    if(!Bt == null){
+//        Log("Bluetooth supported");
+//    }else{
+//        Log("Bluetooth not supported");
+//        finish();
+//    }
+
+    //    Intent enabledBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//    startActivityForResult()
+
+}
---
+++ app/src/main/java/com/example/kazuaki/blewerewolf/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.kazuaki.blewerewolf;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothAdapter.LeScanCallback mLeScanCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //BLE
+        mBluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
+
+        //BLEスキャンした際のコールバック
+        mLeScanCallback = new BluetoothAdapter.LeScanCallback(){
+            @Override
+            public void onLeScan(BluetoothDevice device,int rssi,byte[] scanRecord){
+                String msg = "ADDRESS=" + device.getAddress() + "\nRSSI=" + rssi;
+                Log.d("BLE", msg);
+            }
+        };
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
